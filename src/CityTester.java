3a29b12a352547fb29d3dd0619fd5263527b6034
Seth Miller <sethmiller24@whitworth.edu>
2021-01-14 14:38:47 -0800

    Clean CityTester

--- src/CityTester.java
+++ src/CityTester.java
@@ -5,34 +5,6 @@ public class CityTester {
 		City Bellevue = new City("Bellevue");
 		
 		CityComponent test = new CityComponent(Bellevue);
-		
-		//CityComponentInterfaceThingy test2 = new CityComponentInterfaceThingy(Bellevue);
-		
-		/*
-		for (int i = 0; i < 3; i++) {
-			Bellevue.populateCity(new Building());
-			Bellevue.populateCity(new Person());
-			Bellevue.populateCity(new Kid());
-			Bellevue.populateCity(new Police());
-			Bellevue.populateCity(new Teacher());
-		}
-		
-		Bellevue.populateCity(new Building("McDonald's"));
-		Bellevue.populateCity(new Person ("Phone Number Tester", 25, 1234567));
-		
-		//Test Teacher and Police Constructors
-		Bellevue.populateCity(new Teacher("Mr. Meaty", 4));
-		Bellevue.populateCity(new Police(40,"Mr. Sheriff",20));
-		
-		//Test Kid Constructors
-		Bellevue.populateCity(new Kid("Diet Cookie","Diet Cookie Boy",10));
-		Bellevue.populateCity(new Kid("Big Jelly Bean","Jelly Bean Boy",10));
-		
-		System.out.println(Bellevue.readCityPopulus());
-		System.out.println(Bellevue.payEmployees());
-		System.out.println(Bellevue.payEmployees());
-		*/
-		
 	}
 
 }
